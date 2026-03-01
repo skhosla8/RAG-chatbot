d8b4387a1659511d3a37a91c6548933b66b82556
skhosla8 <59849053+skhosla8@users.noreply.github.com>
2026-02-28 18:09:10 -0600

    update

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -97,7 +97,7 @@ const loadSampleData = async () => {
 
 const scrapePage = async (url: string) => {
     // const executablePath = await chromium.executablePath('/opt/chromium');
-     const executablePath = await chromium.executablePath('https://github.com/Sparticuz/chromium/releases/chromium-v143.0.4-pack.ARCH.tar');
+     const executablePath = await chromium.executablePath('chromium-v143.0.4-pack.ARCH.tar');
      //const execDir = path.dirname(executablePath); // /var/folders/q1/l34cx8cd3cnctr11s2b773dm0000gn/T
 
     // console.log(execDir)
