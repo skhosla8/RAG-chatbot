d2fe0b4fef048a286d29ca8ca4ba14eea3ada320
skhosla8 <59849053+skhosla8@users.noreply.github.com>
2026-03-03 15:01:40 -0600

    update

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -14,6 +14,7 @@ import OpenAI from 'openai';
 import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
 
 import 'dotenv/config';
+import { ppid } from 'process';
 
 export const dynamic = 'force-dynamic';
 
@@ -168,7 +169,7 @@ try {
     // console.log(executablePath)
 
     //process.env.LD_LIBRARY_PATH = execDir;
-    
+
     try {
         let webBrowser;
 
