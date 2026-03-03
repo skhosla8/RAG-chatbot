2e64d825553ab5e12c224eecaa15c6d38e895cda
skhosla8 <59849053+skhosla8@users.noreply.github.com>
2026-03-03 16:16:25 -0600

    update

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -160,8 +160,10 @@ try {
   console.error('Error reading chromium path:', err);
 }
   */ 
+     const executablePath = await chromium.executablePath('https://mahjong-chatbot.s3.us-east-2.amazonaws.com/chromium.br');
 
-     const executablePath = await chromium.executablePath('https://mahjong-chatbot.s3.us-east-2.amazonaws.com/chromium-v143.0.4-pack.x64+(6).tar');
+
+    // const executablePath = await chromium.executablePath('https://mahjong-chatbot.s3.us-east-2.amazonaws.com/chromium-v143.0.4-pack.x64+(6).tar');
 
     // const execDir = path.dirname(executablePath); // /var/folders/q1/l34cx8cd3cnctr11s2b773dm0000gn/T
 
