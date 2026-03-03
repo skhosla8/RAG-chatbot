ff7d6affad438f743c2731297f0c31483a557659
skhosla8 <59849053+skhosla8@users.noreply.github.com>
2026-03-03 15:53:40 -0600

    update

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -161,7 +161,7 @@ try {
 }
   */ 
 
-     const executablePath = await chromium.executablePath('https://mahjong-chatbot.s3.us-east-2.amazonaws.com/chromium');
+     const executablePath = await chromium.executablePath('https://mahjong-chatbot.s3.us-east-2.amazonaws.com/chromium-v143.0.4-pack.x64+(6).tar');
 
     // const execDir = path.dirname(executablePath); // /var/folders/q1/l34cx8cd3cnctr11s2b773dm0000gn/T
 
