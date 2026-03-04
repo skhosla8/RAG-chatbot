e392080f0a7056ffb5e05edd901b3a9ec33692b7
skhosla8 <59849053+skhosla8@users.noreply.github.com>
2026-03-03 19:42:43 -0600

    update

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -170,7 +170,7 @@ try {
     // console.log(execDir)
     // console.log(executablePath)
 
-    process.env.LD_LIBRARY_PATH = '/tmp/chromium';
+   // process.env.LD_LIBRARY_PATH = '/tmp/chromium';
 
     try {
         let webBrowser;
