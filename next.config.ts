4de8d352adeab5307af7ba910aa21952a2381658
skhosla8 <59849053+skhosla8@users.noreply.github.com>
2026-03-04 15:42:52 -0600

    update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverExternalPackages: ["puppeteer-core", "@sparticuz/chromium-min"],
+  //serverExternalPackages: ["puppeteer-core", "@sparticuz/chromium-min"],
+  serverExternalPackages: ["puppeteer-core", "@sparticuz/chromium"]
 }
 
 export default nextConfig;
