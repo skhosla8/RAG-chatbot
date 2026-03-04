bdd60cbbcdcc24e19790910bbee84af8b9102ee2
skhosla8 <59849053+skhosla8@users.noreply.github.com>
2026-03-04 16:36:58 -0600

    update

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -37,7 +37,7 @@ const {
 const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
 
 const mahjongData = [
-    //"https://www.plainfieldlibrary.net/wp-content/uploads/2020/03/American-Mahjongg.pdf",
+    "https://www.plainfieldlibrary.net/wp-content/uploads/2020/03/American-Mahjongg.pdf",
     "https://en.wikipedia.org/wiki/Mahjong#History",
     "https://www.google.com/search?q=mahjong+2025&sca_esv=af25f58d0793347a&biw=1728&bih=888&aic=0&sxsrf=ANbL-n6HUseYKXEdStNIg4sgFOnohXexBA%3A1769989063627&ei=x-N_acuGJvrJkPIP-8qSiQU&ved=0ahUKEwiLhpOLu7mSAxX6JEQIHXulJFE4ChDh1QMIEw&uact=5&oq=mahjong+2025&gs_lp=Egxnd3Mtd2l6LXNlcnAiDG1haGpvbmcgMjAyNTIFEAAYgAQyBRAAGIAEMgUQABiABDIKEAAYgAQYFBiHAjIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABEjiBVCRA1jyBHABeAGQAQCYAWugAbkBqgEDMS4xuAEDyAEA-AEBmAIDoALCAcICDRAjGPAFGLADGCcYngbCAgcQIxiwAxgnwgIKEAAYsAMY1gQYR8ICChAjGIAEGCcYigXCAgoQIxjwBRgnGJ4GwgIEECMYJ8ICChAAGIAEGEMYigXCAgsQABiABBiRAhiKBZgDAIgGAZAGCpIHAzIuMaAHnxGyBwMxLjG4B74BwgcDMC4zyAcGgAgA&sclient=gws-wiz-serp",
     "https://www.scribd.com/document/250898340/Mahjong-Hands-Lisit"
